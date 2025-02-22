b88b1f31c68a4eda44fa1827fb347a16a168c6c6
Anna <annakondr0709a@gmail.com>
2025-02-22 16:17:36 +0300

    Git и случайные окружности

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import random
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtCore import QPoint
 from PyQt6.QtWidgets import QWidget, QApplication
-from PyQt6 import uic
+from UI import Ui_Form
+from random import choice
 
 
-class Example(QWidget):
+class Example(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.setFixedSize(self.size())
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
@@ -19,7 +20,10 @@ class Example(QWidget):
         if self.do_paint:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor(255, 255, 0))
+            r = choice(range(0, 256))
+            g = choice(range(0, 256))
+            b = choice(range(0, 256))
+            qp.setBrush(QColor(r, g, b))
             r = random.randint(1, self.height() // 2)
             x, y = random.randint(r, self.width() - r), random.randint(r, self.height() - r)
             qp.drawEllipse(QPoint(x, y), r, r)
